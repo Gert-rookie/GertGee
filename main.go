5aefe5f9e80a24f413500f8771aa4e51e72977af
honghaoteng <honghaoteng@inke.cn>
2021-09-24 18:12:45 +0800

    day1

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package GertGee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	/*http.HandleFunc("/",indexHandler)
+	http.HandleFunc("/hello",helloHandler)*/
+}
+
+func indexHandler(w http.ResponseWriter, req http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+func helloHandler(w http.ResponseWriter, req http.Request) {
+	for k,v:=range req.Header{
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+}
